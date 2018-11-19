5bfc27aa654160285eea6f2a5501a7fc3a4d56d7
danilopez97 <estu97dani@gmail.com>
2018-11-19 11:37:48 -0600

    se creo el modelo

--- final/admin.py
+++ final/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from final.models import Curso,CursoAdmin,Programa,ProgramaAdmin
 # Register your models here.
+
+admin.site.register(Curso, CursoAdmin)
+admin.site.register(Programa, ProgramaAdmin)
--- final/models.py
+++ final/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+class Curso(models.Model):
+    nombre  =   models.CharField(max_length=250)
+    profesor = models.CharField(max_length=250)
+    descripcion=models.CharField(max_length=250)
+    def __str__(self):
+        return self.nombre
+class Programa(models.Model):
+    estudiante    = models.CharField(max_length=250)
+    nombre_programa   = models.CharField(max_length=250)
+    grado   = models.CharField(max_length=250)
+    anio      = models.IntegerField()
+    cursos   = models.ManyToManyField(Curso, through='Asignacion')
+    def __str__(self):
+        return self.estudiante
+
+
+
+class Asignacion(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    programa = models.ForeignKey(Programa, on_delete=models.CASCADE)
+    nota = models.IntegerField()
+
+class AsignacionInLine(admin.TabularInline):
+    model = Asignacion
+    extra = 1
+
+class CursoAdmin(admin.ModelAdmin):
+    inlines = (AsignacionInLine,)
+
+class ProgramaAdmin(admin.ModelAdmin):
+    inlines = (AsignacionInLine,) 
+ 
