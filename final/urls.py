2438343052716d12373f5ee6bd7f31fc52140e76
danilopez97 <estu97dani@gmail.com>
2018-11-19 12:00:52 -0600

    se crearon las vistas

--- final/urls.py
+++ final/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+        url(r'^pensum/nuevo/$', views.ingresar_pensum, name='ingresar_pensum'),
+
     ]
--- final/views.py
+++ final/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 from django.contrib import messages
-from final.models import Curso,Asignacion
+from final.models import Curso,Asignacion,Programa
+from .forms import ProgramaForm
 
 # Create your views here.
+def ingresar_pensum(request):
+
+    if request.method == "POST":
+
+        formulario = ProgramaForm(request.POST)
+
+        if formulario.is_valid():
+
+            programa = Programa.objects.create(estudiante=formulario.cleaned_data['estudiante'], nombre = formulario.cleaned_data['nombre_programa'],
+            grado=formulario.cleaned_data['grado'], anio=formulario.cleaned_data['anio'])
+
+            for actor_id in request.POST.getlist('cursos'):
+
+                asignacion = Asignacion(curso_id=curso_id, programa_id = programa.id)
+
+                actuacion.save()
+
+            messages.add_message(request, messages.SUCCESS, 'Programa Guardada Exitosamente')
+
+    else:
+
+        formulario = ProgramaForm()
+
+    return render(request, 'final/pensum_ingresar.html', {'formulario': formulario})
